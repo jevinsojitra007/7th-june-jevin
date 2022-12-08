3ded64cad57d5b0beec988bcfef6ea9f9e7128b0
jevinsojitra007 <jevinsojitra007@gmail.com>
2022-12-08 15:38:59 +0530

    Get-set value Method

---
+++ JavaScript/MyJavaScript/(19.1)dom.js
@@ -0,0 +1,55 @@
+// var element;
+
+// element = document.getElementById("header");
+// element = document.getElementsByClassName("list");
+// element = document.getElementsByTagName("ul");
+
+// console.log(element);
+
+////////////////////////////////////////////////////
+
+// var element;
+
+// element = document.getElementById("main").innerHTML
+// element = document.getElementById("main").innerText
+
+// console.log(element)
+
+///////////////////////////////////////////////////
+
+
+// var element;
+
+// element = document.getElementById("main").getAttribute("id");
+// element = document.getElementById("main").getAttribute("class");
+// element = document.getElementById("main").getAttribute("style");
+// element = document.getElementById("main").getAttribute("onClick");
+// element = document.getElementById("main").getAttributeNode("style").value;
+// element = document.getElementById("main").attributes[2].name; 
+
+// console.log(element);
+
+////////////////////////////////////////////////////
+
+// var element;
+// element = document.getElementById("header").innerHTML;
+// console.log(element)
+
+// document.getElementById("header").innerHTML  = " <h1>Hello Everone</h1>"
+
+// element = document.getElementById("header").innerHTML;
+
+// console.log(element)
+////////////////////////////////////////////////////
+
+// var element;
+
+// document.getElementById("header").innerHTML  = " <h1>Hello Everone</h1>";
+
+// document.getElementById("header").setAttribute("style","border:10px dotted yellow;");
+
+// element = document.getElementById("header").setAttribute("class");
+
+// console.log(element); 
+
+///////////////////////////////////////////////
