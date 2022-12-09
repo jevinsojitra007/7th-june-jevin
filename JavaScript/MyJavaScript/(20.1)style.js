713b3d28516839c3927418a8aac7425fbd5ce178
jevinsojitra007 <jevinsojitra007@gmail.com>
2022-12-09 12:47:34 +0530

    Styling-Method-JS

---
+++ JavaScript/MyJavaScript/(20.1)style.js
@@ -0,0 +1,7 @@
+var element;
+
+document.querySelector("#header").style.backgroundColor = "tan";
+document.querySelector("#header").style.color = "purple";
+
+element = document.querySelector("#header").style.color;
+console.log(element)
